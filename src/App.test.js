e17737464b3eac631c8bbbec189eb9266f170583
Caroline Forslund <cf222ev@student.lnu.se>
2020-11-24 11:50:17 +0200

    1 test passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { StoreProvider } from './StoreContext'; 
+import { shallow, mount } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders My Travel Journal heading', () => {
+  render(<StoreProvider><App /></StoreProvider>);
+  const h1 = screen.getByText(/My Travel Journal/i);
+  expect(h1).toBeInTheDocument();
+});
+
+test('renders Add trip button', () => {
+  render(<StoreProvider><App /></StoreProvider>);
+  const button = screen.getByText(/Add new trip/i);
+  expect(button).toBeInTheDocument();
+});
+
+test('change modalOpen to true when button is clicked', () => {
+  const wrapper = render(<StoreProvider><App /></StoreProvider>);
+  wrapper.find('button').simulate('click');
+  expect(wrapper.state('openModal')).toEqual(true);
 });
